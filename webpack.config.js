ebdf652cfdc1d66e7ff182e343da4d22462a9296
Ethan <ethtaylan@gmail.com>
2024-06-30 00:39:46 +0200

    fix: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  node: {
+    __dirname: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        './node_modules/swagger-ui-dist/swagger-ui.css',
+        './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
+        './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
+        './node_modules/swagger-ui-dist/favicon-16x16.png',
+        './node_modules/swagger-ui-dist/favicon-32x32.png',
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
